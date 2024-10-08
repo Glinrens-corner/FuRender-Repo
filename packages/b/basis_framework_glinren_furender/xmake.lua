01c2afe692f46295fe401fef1d6d435ddcbd1e33
glinren <glinren.kre@gmail.com>
2024-10-09 00:33:33 +0200

    changed package to add includedirs

--- packages/b/basis_framework_glinren_furender/xmake.lua
+++ packages/b/basis_framework_glinren_furender/xmake.lua
@@ -4,8 +4,9 @@ package("basis_framework_glinren_furender")
     add_urls("https://github.com/Glinrens-corner/FuRender.git")
     add_versions("0.0.1-pre-alpha", "5a704460d5a778280a5e624a0458c46ac25ebaef")
     on_install(function (package)
-	  --Todo
+       package:add("includedirs", "include")
     end)
     on_load(function (package)
         package:add("includedirs", "include")
     end)
+ 
