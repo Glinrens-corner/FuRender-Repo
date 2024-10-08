e966853cc0f7ba51b657db74ca4001a2406f832f
glinren <glinren.kre@gmail.com>
2024-10-09 00:09:48 +0200

    basic_framework now supports all platforms

--- packages/b/basis_framework_glinren_furender/xmake.lua
+++ packages/b/basis_framework_glinren_furender/xmake.lua
@@ -3,6 +3,9 @@ package("basis_framework_glinren_furender")
 
     add_urls("https://github.com/Glinrens-corner/FuRender.git")
     add_versions("0.0.1-pre-alpha", "5a704460d5a778280a5e624a0458c46ac25ebaef")
+    on_install(function (package)
+	  --Todo
+    end)
     on_load(function (package)
         package:add("includedirs", "include")
     end)
