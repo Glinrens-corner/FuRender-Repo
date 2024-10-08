a4592e768b9ae64be09031e251fcbcdf24be8add
glinren <glinren.kre@gmail.com>
2024-10-09 01:25:05 +0200

    added 0.0.3

--- packages/b/basis_framework_glinren_furender/xmake.lua
+++ packages/b/basis_framework_glinren_furender/xmake.lua
@@ -5,6 +5,7 @@ package("basis_framework_glinren_furender")
 
     add_versions("0.0.1-pre-alpha", "5a704460d5a778280a5e624a0458c46ac25ebaef")
     add_versions("0.0.2-pre-alpha", "354fdb9a16237b793ebb603450e67e6804482fcc")
+    add_versions("0.0.3-pre-alpha", "9873247ca3f43caf58df127ebdcc787199f69a45")
     on_install(function (package)
 	  import("package.tools.xmake").install(package)
     end)
