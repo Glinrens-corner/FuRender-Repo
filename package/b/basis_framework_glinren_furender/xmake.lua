b256213d3693e75de84467259385419b2f9dd53b
Arne Bargholz <glinren@localhost.localdomain>
2024-10-08 16:43:23 +0200

    added the basic framework for FuRender

---
+++ package/b/basis_framework_glinren_furender/xmake.lua
@@ -0,0 +1,8 @@
+package("basis_framework_glinren_furender")
+    set_description("basic Framework of the FuRender UI-framework")
+
+    add_urls("https://github.com/Glinrens-corner/FuRender.git")
+    add_versions("0.0.1-pre-alpha", "5a704460d5a778280a5e624a0458c46ac25ebaef")
+    on_load(function (package)
+        package:add("includedirs", "include")
+    end)
